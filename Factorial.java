03e8d0fc4aa944bc8a1dfad8e725ecd94ef8a26f
Raddios <raddiosw@gmail.com>
2018-12-07 15:46:16 +0200

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,48 @@
+package solution;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		int number;
+		Scanner scan = new Scanner(System.in);
+		number = scan.nextInt();
+		if (number >= 1 && number <= 2_000) {
+			SearchFactorial(number);
+		} else {
+			System.out.println("Wrong number");
+		}
+
+		scan.close();
+
+	}
+
+	private static void SearchFactorial(int number) {
+		switch (number) {
+		case 1:
+			System.out.println("1");
+			break;
+		case 2:
+			System.out.println("2");
+			break;
+		case 6:
+			System.out.println("3");
+			break;
+		case 24:
+			System.out.println("7");
+			break;
+		case 120:
+			System.out.println("5");
+			break;
+		case 720:
+			System.out.println("6");
+			break;
+		default:
+			System.out.println("There is no integer to be the factorial basis");
+			break;
+		}
+		
+	}
+
+}
